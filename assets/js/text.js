9e4972e552c3c9546219d98dbd9f70452ef85b8c
vidanio777 <155305437+vidanio777@users.noreply.github.com>
2024-08-11 09:53:15 +0300

    Add files via upload

---
+++ assets/js/text.js
@@ -0,0 +1,11 @@
+$(function () {
+    $(".typed").typed({
+        strings: ["еееееее я крутой", "Pupsik", "JS and HTML", "Python Developer", "coder", "vzlom jopi", "hmmm...", "bruh", "KR4CK3", "Pashalko", "h@Cк3D!", "52 pashalko"],
+        typeSpeed: 30,
+        backSpeed: 0,
+        cursorChar: ['❚'],
+        smartBackspace: true,
+        fadeOut: true,
+        loop: true,
+    });
+});
